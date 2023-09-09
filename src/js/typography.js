2dad302e63af491726f50945046714101cef8be1
ClaudiusAyadi <ayaclaudius@gmail.com>
2023-09-09 10:02:31 +0100

    [EDIT] Removed font-size script

--- src/js/typography.js
+++ src/js/typography.js
@@ -2,15 +2,10 @@ import fs from "fs";
 import keto from "../../keto.config.js";
 
 const fontFamily = keto.fontFamily;
-const fontSizes = keto.fontSizes;
 
 // Create the SCSS map strings
 const fontFamilyMap = `$fontFamily: (\n${Object.entries(fontFamily)
 	.map(([fontFamily, value]) => `${fontFamily}: ${value}`)
-	.join(",\n")}\n);\n`;
-
-const fontSizeMap = `$fontSizes: (\n${Object.entries(fontSizes)
-	.map(([fontSize, value]) => `${fontSize}: ${value}`)
 	.join(",\n")}\n);\n`;
 
 // Read the existing content of workers.scss
@@ -24,20 +19,16 @@ fs.readFile("src/scss/abstracts/_workers.scss", "utf8", (err, data) => {
 	const fontFamilyRegex = /\$fontFamily:\s*\([\s\S]*?\);/g;
 	const oldFontFamily = data.match(fontFamilyRegex);
 
-	// Find & replace the existing $fontSizes map
-	const fontSizeRegex = /\$fontSizes:\s*\([\s\S]*?\);/g;
-	const oldFontSize = data.match(fontSizeRegex);
-
-	// Append the new $fontFamily and $fontSizes maps to workers.scss
-	const newFonts = data.replace(oldFontFamily, "").replace(oldFontSize, "").trim();
-	const newFontMap = newFonts + "\n" + fontFamilyMap + "\n" + fontSizeMap + "\n";
+	// Append the new $fontFamily map to workers.scss
+	const newFontFamily = data.replace(oldFontFamily, "").trim();
+	const newFontFamilyMap = newFontFamily + "\n" + fontFamilyMap;
 
 	// Write the final content back to workers.scss
-	fs.writeFile("src/scss/abstracts/_workers.scss", newFontMap, (writeErr) => {
+	fs.writeFile("src/scss/abstracts/_workers.scss", newFontFamilyMap, (writeErr) => {
 		if (writeErr) {
-			console.error("Error adding fonts to workers.scss:", writeErr);
+			console.error("Error adding Font Family to workers.scss:", writeErr);
 		} else {
-			console.log("Font maps have been updated in workers.scss!");
+			console.log("Font Family maps have been updated in workers.scss!");
 		}
 	});
 });
