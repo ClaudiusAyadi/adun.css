3a227a2a6dab04c2b14b6034a9e73de8bcd4df20
ClaudiusAyadi <ayaclaudius@gmail.com>
2023-09-09 17:39:51 +0100

    [ADDED] More helper classes.

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -2,11 +2,19 @@ const postcss = require("postcss");
 const postcssSass = require("@csstools/postcss-sass");
 const purgeCSS = require("@fullhuman/postcss-purgecss");
 const cssnano = require("cssnano");
+const pxtorem = require("@minko-fe/postcss-pxtorem");
 
 module.exports = {
 	plugins: [
 		postcss([postcssSass()]).process("src/scss/app.scss"),
 		require("autoprefixer"),
+		pxtorem({
+			rootValue: 16,
+			selectorBlackList: ["some-class"],
+			propList: ["*"],
+			atRules: ["media"],
+			// ...
+		}),
 		purgeCSS({
 			content: [
 				"./**/*.{html,js,jsx,ts,tsx,php}",
